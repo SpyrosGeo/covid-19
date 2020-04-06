41b9802fe800ac288e619708b25e8db1570b7c9c
SpyrosGeo <xarodasimba@gmail.com>
2020-04-06 16:34:27 +0300

    implemented the Chart component

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {fetchDailyData} from '../../api'
+import {Line,Bar} from 'react-chartjs-2';
+
+
+import styles from './Chart.module.css'
 
 export default function Chart() {
+const [dailyData,setDailyData] = useState({})
+
+
+useEffect(()=>{
+const fetchAPI = async()=>{
+    const initialDailyData = await fetchDailyData();
+    // console.log(initialDailyData)
+    setDailyData(initialDailyData);
+}
+fetchAPI()
+},[])
+// console.log("daily",dailyData)
+const lineChart = dailyData[0] ? (
+  <Line
+    data={{
+      labels: dailyData.map(({ date }) => date),
+      datasets: [
+        {
+          data: dailyData.map((data) => data.confirmed),
+          label: "Infected",
+          borderColor: "#3333ff",
+          fill: true,
+        },
+        {
+          data: dailyData.map((data) => data.deaths),
+          label: "Deaths",
+          borderColor: "red",
+          backgroundColor: "rgba(255, 0, 0, 0.5)",
+          fill: true,
+        },
+      ],
+    }}
+  />
+) : null;
+
+
+
     return (
-        <div>
-         <h1>Chart</h1>   
+        <div className={styles.container}>
+           {lineChart}
         </div>
     )
 }
